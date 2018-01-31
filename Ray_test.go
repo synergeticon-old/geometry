ecdc9bcea25f8b101595d15b094b199e363585f1
Daniel Erdelmeier <d.erdelmeier@synergeticon.de>
2018-01-31 16:08:29 +0100

    test

---
+++ Ray_test.go
@@ -0,0 +1,24 @@
+package geometry_test
+
+import (
+	"testing"
+
+	"github.com/gonum/matrix/mat64"
+
+	"github.com/synergeticon/geometry"
+)
+
+func TestRayDistanceToPoint(t *testing.T) {
+
+	v1 := mat64.NewVector(3, []float64{0, 0, 0})
+	v2 := mat64.NewVector(3, []float64{1, 0, 0})
+	point := mat64.NewVector(3, []float64{0, 1, 0})
+
+	ray := geometry.NewRayFromPoints(v1, v2)
+
+	distance := ray.DistanceToPoint(point)
+	if distance != 1 {
+		t.Log("Should be 1, but is")
+		t.Fail()
+	}
+}
